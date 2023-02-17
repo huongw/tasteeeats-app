add089e443a85e9886a71eb3d223bf732583ab01
Whitney <designedbywh@gmail.com>
2023-02-17 11:51:35 -0500

    installed libraries and implement popular component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import Home from "./pages/Home";
 import { Cuisine, Searched, Recipe } from "./pages";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/cuisine/:type" element={<Cuisine/>}/>
-          <Route path="/searched/:search" element={<Searched/>}/>
-          <Route path="/recipe/:name" element={<Recipe/>}/>
-        </Routes>
-      </BrowserRouter>
+      <Wrapper>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cuisine/:type" element={<Cuisine />} />
+            <Route path="/searched/:search" element={<Searched />} />
+            <Route path="/recipe/:name" element={<Recipe />} />
+          </Routes>
+        </Router>
+      </Wrapper>
     </div>
   );
 }
+
+const Wrapper = styled.div`
+  width: 90%;
+  max-width: 1500px;
+  margin: auto;
+`;
 
 export default App;
--- src/components/MostPopular.jsx
+++ src/components/MostPopular.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 function MostPopular() {
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    const popularItems = localStorage.getItem("popular");
+
+    if (popularItems) {
+      setPopular(JSON.parse(popularItems));
+      return;
+    }
+    axios
+      .get(`/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        localStorage.setItem("popular", JSON.stringify(res.data.recipes));
+        setPopular(res.data.recipes);
+      });
+  }, []);
+
   return (
-    <div>MostPopular</div>
-  )
+    <Wrapper>
+      <h3>Most Popular</h3>
+      <Splide
+        options={{
+          perPage: 3,
+          arrows: true,
+          pagination: false,
+          drag: "free",
+          gap: "1rem",
+        }}
+      >
+        {popular.map((recipe) => {
+          return (
+            <SplideSlide key={recipe.id}>
+              <Link to={`/recipe/${recipe.id}`}>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
+                </Card>
+              </Link>
+            </SplideSlide>
+          );
+        })}
+      </Splide>
+    </Wrapper>
+  );
 }
 
-export default MostPopular
+const Wrapper = styled.div`
+  h3 {
+    margin: 1rem 0;
+  }
+`;
+
+const Card = styled.div`
+  border-radius: 25px;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    width: 100%;
+  }
+
+  p {
+    position: absolute;
+    bottom: 0;
+    color: #fff;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 2;
+    margin: 1rem 0;
+    text-align: center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
+
+export default MostPopular;
